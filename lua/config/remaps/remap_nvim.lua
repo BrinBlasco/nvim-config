5af090740168284c82cbe080f41b0d48eaa8d29a
Brin Blasco <brin.osebno@gmail.com>
2024-12-04 23:59:39 +0100

    ...

--- lua/config/remaps/remap_nvim.lua
+++ lua/config/remaps/remap_nvim.lua
@@ -2,3 +2,9 @@
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("i", "jk", "<Esc>:w<Enter>")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "<leader>pw", function ()
+    local symbol = vim.fn.input("Enter symbol: ")
+    local combined_input = "c" .. symbol .. '<C-r>"'
+    vim.api.nvim_input(combined_input)
+end)
