649690935b2848e31a91fc19b07a6aeeaf1b1e94
Brin Blasco <brin.osebno@gmail.com>
2024-12-04 21:32:01 +0100

    ...

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,4 +3,37 @@ require('config.sets')
 
 if not vim.g.vscode then
     require('config.lazy')
+
+    local augroup = vim.api.nvim_create_augroup
+    local autocmd = vim.api.nvim_create_autocmd
+
+    local brin_group = augroup('Brin', {})
+    local yank_group = augroup('Hightlight Yank', {})
+
+
+    autocmd({"BufWritePre"}, {
+        group = brin_group,
+        pattern = "*",
+        command = [[%s/\s\+$//e]],
+    })
+
+    autocmd('TextYankPost', {
+        group = yank_group,
+        pattern = '*',
+        callback = function()
+            vim.highlight.on_yank({
+                higroup = 'IncSearch',
+                timeout = 100,
+            })
+        end,
+    })
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+        group = brin_group,
+        pattern = "*",
+        callback = function()
+            vim.cmd("lcd " .. vim.fn.expand("%:p:h"))
+        end,
+    })
+
 end
